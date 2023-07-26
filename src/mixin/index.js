7b9807a3316c42bcda5a5f9898a42a0859e3d9d8
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2023-07-26 09:29:32 +0100

    - CHG: Version bump before last regular release. - ADD: Added changelog information for new release.

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -3,7 +3,7 @@ import { mapGetters } from 'vuex'
 export default {
   data: function () {
     return {
-      gridScoreVersion: '1.15.0',
+      gridScoreVersion: '1.16.0',
       multiTraitMethods: {
         // TODO: Handle dates!
         last: {
